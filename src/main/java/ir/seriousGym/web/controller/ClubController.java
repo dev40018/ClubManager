2d711296689e2353e3fd5bd84ea59b72c1dc3e8d
dev40018 <g87942131@gmail.com>
2024-09-15 16:30:26 +0330

    working-fine, we added thymeleaf security logic into clubContorller

--- src/main/java/ir/seriousGym/web/controller/ClubController.java
+++ src/main/java/ir/seriousGym/web/controller/ClubController.java
@@ -87,7 +87,14 @@ public class ClubController {
   @GetMapping("/clubs/{clubId}")
   public String showClubDetail(@PathVariable("clubId") long clubId,
       Model model) {
+    UserEntity user = new UserEntity();
     ClubDto clubDto = clubService.findClubById(clubId);
+    String username = SecurityUtil.getUserBySession();
+    if(username != null){
+      user = userService.findByUsername(username);
+      model.addAttribute("user", user);
+    }
+    model.addAttribute("user", user);
     model.addAttribute("club", clubDto);
     return "clubs-detail";
   }
--- src/main/java/ir/seriousGym/web/security/SecurityConfig.java
+++ src/main/java/ir/seriousGym/web/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                                 "/login",
                                 "/register",
                                 "/clubs",
+                                "/clubs/{clubId}",
                                 "/register/**",
                                 "/css/**",
                                 "/js/**",
